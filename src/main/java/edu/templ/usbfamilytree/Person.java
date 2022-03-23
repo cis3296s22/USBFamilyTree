a6c76a79c6402a70a0a1b28ca4de6c0b11460b3f
kalyniy <bohdan.k2000@gmail.com>
2022-03-23 13:49:57 -0400

    Added Person class

---
+++ src/main/java/edu/templ/usbfamilytree/Person.java
@@ -0,0 +1,11 @@
+package edu.templ.usbfamilytree;
+
+public class Person {
+    private String name;
+    public Person(){
+
+    }
+    public Person(String name){
+        this.name = name;
+    }
+}
