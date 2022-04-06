28e604c005312bec5224371c5d0d64246f95ccbd
Fahim Bashar <89543299+tuk93846@users.noreply.github.com>
2022-04-06 01:59:09 -0400

    BFS but needs to be updated

--- src/main/java/edu/templ/usbfamilytree/Graph.java
+++ src/main/java/edu/templ/usbfamilytree/Graph.java
@@ -1,4 +1,4 @@
-package edu.templ.usbfamilytree;package edu.templ.usbfamilytree;
+package edu.templ.usbfamilytree;
 
 
 import java.util.*;
@@ -7,6 +7,8 @@ class Graph<T>
 {
     // creating an object of the Map class that stores the edges of the graph
     private Map<Node, List<Node>> map = new HashMap<>();
+    private int V = Integer.MAX_VALUE - 5; //number of nodes/vertices
+    ArrayList<Integer>[] Adj = new ArrayList[V]; // adjacency list but in integer form for BFS
 
     // the method adds a new vertex to the graph
     public void addNewVertex(Node s)
@@ -45,8 +47,11 @@ class Graph<T>
             destination.addEdge(marital, !ancestor, source.getID());
         }
 
-    }
+        //integer adjacency list
+        Adj[source.getID()].add(destination.getID());
+        Adj[destination.getID()].add(source.getID());
 
+    }
 
     public void containsVertex(Node s) {
         if (map.containsKey(s)) {
@@ -65,6 +70,25 @@ class Graph<T>
         }
     }
 
+
+
+
+    void BFS(ArrayList<Integer> queue, boolean[] visited, int[] parent)
+    {
+        int current = queue.remove(0);
+        for(int i = 0;i < Adj[current].size();i++)
+        {
+            int x = Adj[current].get(i);
+            if(!visited[x])
+            {
+                queue.add(x);
+                visited[x] = true;
+                parent[x] = current;
+            }
+        }
+    }
+
+
     //adjacency list - write code to store nodes according to IDs
     @Override
     public String toString() {
@@ -80,5 +104,64 @@ class Graph<T>
         }
         return (builder.toString());
     }
-}
 
+
+
+
+
+    String BidirectionalSearchPath(Graph G, int src, int dest)
+    {
+        String path = "";
+        boolean[] Visited1 = new boolean[V];
+        boolean[] Visited2 = new boolean[V];
+        int[] parent1 = new int[V];
+        int[] parent2 = new int[V];
+        for(int i=0;i<V;i++){
+            Visited1[i] = false;
+            Visited2[i] = false;
+            parent1[i] = -1;
+            parent2[i] = -1;
+        }
+        ArrayList<Integer> queue1 = new ArrayList<>();
+        ArrayList<Integer> queue2 = new ArrayList<>();
+        queue1.add(src);
+        Visited1[src]=true;
+        queue2.add(dest);
+        Visited2[dest]=true;
+        int intersection = -1;
+        while(queue1.size() > 0 && queue2.size() > 0 && intersection == -1)
+        {
+            BFS(queue1, Visited1, parent1);
+            BFS(queue2, Visited2, parent2);
+
+            for(int i = 0;i < V;i++)
+            {
+                if(Visited1[i] && Visited2[i])
+                {
+                    intersection = i;
+                    break;
+                }
+            }
+        }
+
+        if(intersection == -1)
+            return "";
+
+        String path1 = "";
+        int j = intersection;
+        while(j != -1){
+            path1 = j + " " + path1;
+            j = parent1[j];
+        }
+        String path2 = "";
+        j=parent2[intersection];
+        while(j!=-1)
+        {
+            path2=path2+j+" ";
+            j=parent2[j];
+        }
+        path = path1 + path2;
+        return path;
+    }
+
+}
