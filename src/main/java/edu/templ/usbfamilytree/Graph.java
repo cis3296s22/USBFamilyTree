44f9621cf27ca5fc139f888e45b81c75eba1f46d
Fahim Bashar <89543299+tuk93846@users.noreply.github.com>
2022-04-06 14:04:09 -0400

    graph changed

--- src/main/java/edu/templ/usbfamilytree/Graph.java
+++ src/main/java/edu/templ/usbfamilytree/Graph.java
@@ -3,13 +3,16 @@ package edu.templ.usbfamilytree;
 
 import java.util.*;
 
-class Graph<T>
+class Graph
 {
     // creating an object of the Map class that stores the edges of the graph
     private Map<Node, List<Node>> map = new HashMap<>();
     private int V = Integer.MAX_VALUE - 5; //number of nodes/vertices
     ArrayList<Integer>[] Adj = new ArrayList[V]; // adjacency list but in integer form for BFS
+    public Graph(){
 
+
+    }
     // the method adds a new vertex to the graph
     public void addNewVertex(Node s)
     {
