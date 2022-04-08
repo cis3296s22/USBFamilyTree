ad4e278cde02ac56125e3aadc6be1d356e59b6b9
kalyniy <bohdan.k2000@gmail.com>
2022-04-07 23:24:29 -0400

    Get Person Info from prompt

--- src/main/java/edu/templ/usbfamilytree/PersonController.java
+++ src/main/java/edu/templ/usbfamilytree/PersonController.java
@@ -3,14 +3,26 @@ package edu.templ.usbfamilytree;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class PersonController {
+    public Person person;
     @FXML
     Button submitButton;
-
+    @FXML
+    TextField nameTextField;
+    @FXML
+    DatePicker birthdayDatePicker;
+    @FXML
+    TextField occupationTextField;
     @FXML
     private void submit(ActionEvent event){
+        String name = nameTextField.getText();
+        String birthday = birthdayDatePicker.getValue().toString();
+        String occupation = occupationTextField.getText();
+        person = new Person(name, birthday, occupation);
         Stage stage = (Stage) submitButton.getScene().getWindow();
         stage.close();
     }
