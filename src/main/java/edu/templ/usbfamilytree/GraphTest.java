ca740694bcd028a1483acbc447d7d6961dc3fe69
Fahim Bashar <89543299+tuk93846@users.noreply.github.com>
2022-04-12 21:40:00 -0400

    testing starter file

---
+++ src/main/java/edu/templ/usbfamilytree/GraphTest.java
@@ -0,0 +1,45 @@
+package edu.templ.usbfamilytree;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import java.util.*;
+
+
+
+public class GraphTest {
+//    Graph g;
+//
+//    @Before
+//    public void setup()
+//    {
+//        g = new Graph();
+//        g.addNode(0);
+//        g.addNode(1);
+//        g.addNode(2);
+//        g.addNode(3);
+//        g.addNode(4);
+//        g.addNode(5);
+//        g.addNode(6);
+//        g.addNode(7);
+//        g.addNewEdge(0, 2, false, true);
+//        g.addNewEdge(2, 1, true, false);
+//        g.addNewEdge(2, 5, false, true);
+//        g.addNewEdge(2, 6, false, true);
+//        g.addNewEdge(0, 3, false, true);
+//        g.addNewEdge(3, 4, true, false);
+//        g.addNewEdge(3, 7, false, true);
+//    }
+//
+//
+//    @Test
+//    public void zerotosix()
+//    {
+//
+//        ArrayList<Integer> test1 = new ArrayList<>();
+//        ArrayList<Integer> results1 = g.Bidirectional(0, 6);
+//        assertEquals("Should be [0, 2, 6]", test1, results1);
+//
+//    }
+
+}
