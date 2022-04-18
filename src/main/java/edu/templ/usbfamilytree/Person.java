41a986dfdb7c131e609b9fbe015b9bdc6ebb87c8
Fahim Bashar <89543299+tuk93846@users.noreply.github.com>
2022-04-18 18:38:27 -0400

    commented for javadoc

--- src/main/java/edu/templ/usbfamilytree/Person.java
+++ src/main/java/edu/templ/usbfamilytree/Person.java
@@ -14,10 +14,23 @@ public class Person {
 
     }
 
+    /**
+     * A Person can be initialized with just their name if no other data is available
+     * @param name takes individual's name
+     */
     public Person(String name){
         this.name = name;
     }
 
+    /**
+     * If more information is available, this constructor will handle that
+     * @param name Person's name
+     * @param dateOfBirth Person's date of birth
+     * @param occupation Person's job or occupation
+     * @param eyeColor Person's eye color
+     * @param height Person's height
+     * @param filePath Person's path to access their data
+     */
     public Person(String name, String dateOfBirth, String occupation, String eyeColor, String height, String filePath){
         this.name = name;
         this.dateOfBirth = dateOfBirth;
